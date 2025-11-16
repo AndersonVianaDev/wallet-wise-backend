039ab967a2d48598e5b64ceceaf785788eb5a98b
AndersonDev <andersonp.viana.contato@gmail.com>
2025-11-16 15:42:20 -0300

    feat: project created

---
+++ src/main/java/com/anderson/wallet_wise/WalletWiseApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.wallet_wise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletWiseApplication.class, args);
+	}
+
+}
